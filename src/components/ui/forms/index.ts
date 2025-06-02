cad76e87d0a6993fff0933df1b6a07c19409598a
Anderson Chiesa <80640593+Andchiesa@users.noreply.github.com>
2025-06-02 20:22:20 -0300

    Update index.ts

--- src/components/ui/forms/index.ts
+++ src/components/ui/forms/index.ts
@@ -1,1 +1,6 @@
+export { default as InputField } from './InputField';
+export { default as SelectField } from './SelectField';
+export { default as CheckboxField } from './CheckboxField';
+export { default as FormStep } from './FormStep';
+export { default as BusinessForm } from './BusinessForm';
 
